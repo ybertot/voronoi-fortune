3fca1eb11d0e3d6bfa62e34c03790046b4bb47f7
Yves Bertot <Yves.Bertot@inria.fr>
2021-05-29 14:08:46 +0200

    no sign condiion on p1 and p2

--- sqrt_ivt.v
+++ sqrt_ivt.v
@@ -218,8 +218,7 @@ have /noroots p2cn0 := cinar.
 have signs := no_roots_constant_sign cinar noroots.
 have [p2clt0 | ] := boolP (p2.[c] < 0).
   have p2le0' : forall x, a <= x <= r -> p2.[x] <= 0.
-    move=> x intx.
-    have := no_roots_constant_sign cinar noroots intx.
+    move=> x intx; have := signs _ intx.
     by rewrite nmulr_lge0 ?sgr_le0 // sgr_lt0.
   have p1ge0' : forall x, a <= x <= r -> 0 <= p1.[x].
     by move=> x /andP[] alex xler; apply p1ge0; rewrite alex (le_trans xler).
@@ -238,3 +237,129 @@ apply: le_anti; apply/andP; split; first by case/andP: sign_condition.
 by rewrite addr_ge0 ?p1ge0 // ?lexx ?aleb // mulr_ge0 // sqrtr_ge0.
 Qed.
 
+Lemma sqrt_ivt_step1N  (p1 p2 p3 : {poly R}) (a b : R):
+   (forall x, a <= x <= b -> p1.[x] <= 0) ->
+   (forall x, a <= x <= b -> 0 <= p3.[x]) ->
+   a <= b -> p1.[a] + p2.[a] * Num.sqrt p3.[a] <= 0 <=
+             p1.[b] + p2.[b] * Num.sqrt p3.[b] ->
+   exists2 x, a <= x <= b &
+       p1.[x] + p2.[x] * Num.sqrt p3.[x] = 0.
+Proof.
+move=> p1le0 p3ge0 aleb sign_condition.
+set p1' := -p1 \Po (-'X).
+set p2' := -p2 \Po (-'X).
+set p3' := p3 \Po (-'X).
+have aleb' : -b <= -a by rewrite ler_oppr opprK.
+have p1'ge0 : forall x, -b <= x <= -a -> 0 <= p1'.[x].
+  move=> x /andP[]; rewrite ler_oppl ler_oppr => mxleb alemx.
+  rewrite /p1' horner_comp !hornerE ler_oppr oppr0.
+  by apply: p1le0; rewrite mxleb alemx.
+have p3'ge0 : forall x, -b <= x <= -a -> 0 <= p3'.[x].
+  move=> x /andP[]; rewrite ler_oppl ler_oppr => mxleb alemx.
+  rewrite /p3' horner_comp !hornerE.
+  by apply: p3ge0; rewrite mxleb alemx.
+have peq x : p1.[x] + p2.[x] * Num.sqrt p3.[x] =
+       -(p1'.[-x] + p2'.[-x] * Num.sqrt p3'.[-x]).
+  by rewrite /p1' /p2' /p3' !horner_comp !hornerE !mulNr -!opprD !opprK.
+have sign_condition' :
+  p1'.[-b] + p2'.[-b] * Num.sqrt p3'.[-b] <= 0 <=
+  p1'.[-a] + p2'.[-a] * Num.sqrt p3'.[-a].
+  by rewrite -oppr0 ler_oppl ler_oppr -!peq andbC.
+have [r Pr1 Pr2]:= sqrt_ivt_step1 p1'ge0 p3'ge0 aleb' sign_condition'.
+exists (-r); first by rewrite andbC ler_oppl ler_oppr.
+by rewrite peq opprK Pr2 oppr0.
+Qed.
+
+Lemma sqrt_ivt_step2 (p1 p2 p3 : {poly R})(a b : R) :
+  (forall x : R, a <= x <= b -> 0 <= p3.[x]) ->
+       a <= b ->
+       p1.[a] + p2.[a] * Num.sqrt p3.[a] <= 0 <=
+       p1.[b] + p2.[b] * Num.sqrt p3.[b] ->
+       exists2 x : R, a <= x <= b & p1.[x] + p2.[x] * Num.sqrt p3.[x] = 0.
+Proof.
+move=> p3ge0 aleb sign_condition.
+move: aleb; rewrite le_eqVlt=>/orP[/eqP aisb | altb].
+  exists a; first by rewrite aisb !lexx.
+  by apply: le_anti; case/andP: sign_condition; rewrite aisb => ->.
+case: (rootsP p1 a b) (path_roots p1 a b).
+  move=> p10 _ _.
+  have/(fun h => sqrt_ivt_step1 h p3ge0 (ltW altb) sign_condition) :
+    forall x, a <= x <= b -> 0 <= p1.[x].
+    by move=> x _; rewrite p10 hornerE lexx.
+  by rewrite p10.
+move: (roots p1 a b)=> l.
+elim: l a b p3ge0 sign_condition altb => [ | r rts Ih]
+   a b p3ge0 sign_condition altb p1n0 rson sseq pseq.
+set c := (a + b) / 2%:R.
+have altc : a < c.
+  by rewrite /c ltr_pdivl_mulr ?ltr0Sn // mulr_natr mulr2n ltr_add2l.
+have cltr : c < b.
+  by rewrite /c ltr_pdivr_mulr ?ltr0Sn // mulr_natr mulr2n ltr_add2r.
+have cinab : c \in `]a, b[ by apply/andP.
+have cstsg := no_roots_constant_sign cinab (roots_on_nil rson).
+have [ | ] := boolP(p1.[c] < 0).
+  rewrite -sgr_lt0=> p1clt0.
+  have p1le0 : forall x, a <= x <= b -> p1.[x] <= 0.
+    by move=> x xi; have := cstsg x xi; rewrite (nmulr_lge0 _ p1clt0) sgr_le0.
+  by apply: sqrt_ivt_step1N; rewrite // ltW.
+  rewrite -leNgt le_eqVlt -sgr_gt0=> /orP[abs | p1cgt0].
+    by have := roots_on_nil rson cinab; rewrite /root eq_sym abs.
+  have p1ge0 : forall x, a <= x <= b -> 0 <= p1.[x].
+    by move=> x xi; have := cstsg x xi; rewrite (pmulr_lge0 _ p1cgt0) sgr_ge0.
+  by apply: sqrt_ivt_step1; rewrite // ltW.
+have rltb : r < b.
+  by move/roots_on_in: rson=> /(_ r); rewrite inE eqxx=> /(_ isT)/andP[].
+have [prle0 | ] := boolP(p1.[r] + p2.[r] * Num.sqrt p3.[r] <= 0).
+  have altr : a < r by move: pseq; rewrite /= => /andP[].
+  have p3ge0' : forall x, r <= x <= b -> 0 <= p3.[x].
+    move=> x /andP[] rlex xleb; apply: p3ge0.
+    by rewrite (le_trans (ltW altr) rlex) xleb.
+  have srts : sorted <%R rts.
+    by move:pseq; rewrite /= => /andP[] _; apply: path_sorted.
+  have prts : path <%R r rts.
+    by move: pseq; rewrite /= => /andP[].
+  have sign_condition' : p1.[r] + p2.[r] * Num.sqrt p3.[r] <= 0 <=
+     p1.[b] + p2.[b] * Num.sqrt p3.[b].
+    by rewrite prle0; case/andP: sign_condition.
+  have [r' /andP[rler' r'leb] Pr2] := Ih r b p3ge0' sign_condition' rltb p1n0
+      (roots_on_cons sseq rson) srts prts.
+  by exists r'; rewrite // r'leb (le_trans (ltW altr) rler').
+rewrite -ltNge=> prgt0.
+have sign_condition' : p1.[a] + p2.[a] * Num.sqrt p3.[a] <= 0 <=
+                       p1.[r] + p2.[r] * Num.sqrt p3.[r].
+  by rewrite (ltW prgt0) andbT; case/andP: sign_condition.
+have altr : a < r by move: pseq => /andP[].
+set c := (a + r) / 2%:R.
+have altc : a < c.
+  by  rewrite /c ltr_pdivl_mulr ?ltr0Sn // mulr_natr mulr2n ltr_add2l.
+have cltr : c < r.
+  by rewrite /c ltr_pdivr_mulr ?ltr0Sn // mulr_natr mulr2n ltr_add2r.
+have noroots : {in `]a, r[, forall x, ~~root p1 x}.
+  move=> x xinar; move: (xinar)=> /andP[] altx xltr; apply/negP=> abs.
+  have xinab : x \in `]a, b[.
+    by apply/andP; split=> //; apply: (lt_trans xltr rltb).
+  move: (pseq); rewrite /= => /andP[] _.
+  rewrite (path_sortedE lt_trans r rts)=> /andP[] allgt _.
+  move: (root_roots_on rson xinab abs); rewrite inE=> /orP[/eqP xisr | ].
+    have : x < r by exact: xltr.
+    by rewrite xisr ltxx.
+  by move/(allP allgt); rewrite ltNge le_eqVlt orbC; have -> : x < r.
+have cinar : c \in `]a, r[ by apply/andP.
+have signs := no_roots_constant_sign cinar noroots.
+have p3ge0' : forall x, a <= x <= r -> 0 <= p3.[x].
+  move=> x /andP[] alex xler; apply: p3ge0.
+  by rewrite alex (le_trans xler (ltW rltb)).
+suff [r' /andP [aler' r'ler] P2] :
+ exists2 x, a <= x <= r & p1.[x] + p2.[x] * Num.sqrt p3.[x] = 0.
+  by exists r'; rewrite // aler' (le_trans r'ler (ltW rltb)).
+have [ | ] := boolP(p1.[c] < 0).
+  rewrite -sgr_lt0 => p1clt0.
+  have p1le0 : forall x, a <= x <= r -> p1.[x] <= 0.
+    by move=> x xi; rewrite -sgr_le0 -(nmulr_lge0 _ p1clt0) signs.
+  by apply: sqrt_ivt_step1N=> //; apply: ltW.
+rewrite -leNgt le_eqVlt eq_sym -sgr_gt0 => /orP[abs | p1cgt0].
+  by have := noroots _ cinar; rewrite /root abs.
+have p1le0 : forall x, a <= x <= r -> 0 <= p1.[x].
+  by move=> x xi; rewrite -sgr_ge0 -(pmulr_lge0 _ p1cgt0) signs.
+by apply: sqrt_ivt_step1=> //; apply: ltW.
+Qed.
